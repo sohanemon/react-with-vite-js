45e65d471b4e004027685d0610b40d9c8a1d7a77
sohanemon <m.sohanemon@gmail.com>
2022-10-02 21:45:03 +0600

    z index applied

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,10 +5,12 @@ const Navbar = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
   return (
     <section>
-      <div>
-        <h1 className='bg-gray-600 text-center  text-xl font-bold'>Vite test</h1>
+      <div className='z-10'>
+        <h1 className='bg-gray-600 text-center  text-xl font-bold'>
+          Vite test
+        </h1>
       </div>
-      <ul className='text-gray-900 bg-gray-600 '>
+      <div className='text-gray-900 bg-gray-600 '>
         <div
           className='self-start p-5 text-2xl md:hidden cursor-pointer'
           onClick={() => setIsNavOpen((p) => !p)}
@@ -16,8 +18,8 @@ const Navbar = () => {
           {isNavOpen ? <MdClose /> : <GiHamburgerMenu />}
         </div>
 
-        <div
-          className={`flex absolute ${
+        <ul
+          className={`flex absolute z-10 ${
             isNavOpen ? "top-18" : "-top-96"
           } flex-col bg-gray-600 md:static w-full md:flex-row justify-center select-none gap-10 py-4 items-center mx-auto font-semibold  duration-500 ease-in `}
         >
@@ -26,8 +28,8 @@ const Navbar = () => {
               <a href={`/${el}`}>{el || "home"}</a>
             </li>
           ))}
-        </div>
-      </ul>
+        </ul>
+      </div>
     </section>
   );
 };
